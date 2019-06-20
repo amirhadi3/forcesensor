54ea783708c6b4f74a137c07edd7b5f8a44be9af
dgblack <davidgblack1@gmail.com>
2019-06-20 08:40:14 -0700

    gui working without plotting

--- src/force_sensor_interface.py
+++ src/force_sensor_interface.py
@@ -6,24 +6,27 @@ from forcesensor.msg import UserCommand
 from forcesensor.msg import ByteMsg
 from serial_port_wrapper import Serial_Device
 from forcesensor import Sensor
-from PySide2.QtWidgets import (QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QRadioButton)
+from PySide2.QtWidgets import (QApplication, QLineEdit, QPushButton, QVBoxLayout, QWidget, QRadioButton)
 from PySide2.QtCore import Slot, Qt
-from PySide2.QtGui import QPainter, QColor
+from PySide2.QtGui import QDoubleValidator
 from pyqtgraph.widgets.MatplotlibWidget import MatplotlibWidget
 import numpy as np
 
 class  gui(QWidget):
 	def __init__(self, sensor, app):
 		QWidget.__init__(self)
+		rospy.init_node('force_sensor_gui')
+
 		self.sensor = sensor
 		self.transducer_num = 1
 		self.num = 0
 		self.measurement = None
 
+		self.lrLayout = QHBoxLayout()
 		self.layout = QVBoxLayout()
 		self.window = QWidget()
 
-		if sensor == 2: #sensor.num_sensors == 2:
+		if sensor.num_sensors == 2:
 			self.device_num_selector = QRadioButton('Click for sensor 2, leave unchecked for sensor 1')
 			self.device_num_selector.toggled.connect(self.device_num_selector_callback)
 			self.layout.addWidget(self.device_num_selector)
@@ -56,33 +59,57 @@ class  gui(QWidget):
 		self.reset_dac_button.clicked.connect(self.reset_dac_wrapper)
 		self.layout.addWidget(self.reset_dac_button)
 
+		self.sampling_rate_input = QLineEdit()
+		self.sampling_rate_input.setValidator(QDoubleValidator(0.1, 1500, 3))
+		self.sampling_rate_input.setPlaceholderText('Enter Sampling Rate (Hz) (<1500Hz only)')
+		self.sampling_rate_input.returnPressed.connect(self.update_sample_rate)
+		self.layout.addWidget(self.sampling_rate_input)
+
+		self.lrLayout.addLayout(self.layout)
+
 		self.pltwidget1 = MatplotlibWidget()
+		self.pltwidget1.getFigure().suptitle('Sensor 1 Output',fontsize=16)
 		self.diffs1 = self.pltwidget1.getFigure().add_subplot(311)
+		self.diffs1.set_title('Differential Signal',fontsize=10)
+		self.diffs1.set_ylabel('Volts')
 		self.sums1 = self.pltwidget1.getFigure().add_subplot(312)
+		self.sums1.set_title('Sum Signal',fontsize=10)
+		self.sums1.set_ylabel('Volts')
 		self.imu1 = self.pltwidget1.getFigure().add_subplot(313)
+		self.imu1.set_title('IMU Data',fontsize=10)
+		self.imu1.set_ylabel('Volts')
 		self.diff_data1 = np.zeros((1,6))
 		self.sum_data1 = np.zeros((1,6))
 		self.imu_data1 = np.zeros((1,3))
-		self.layout.addWidget(self.pltwidget1)
+		self.lrLayout.addWidget(self.pltwidget1)
 
 		self.pltwidget2 = MatplotlibWidget()
+		self.pltwidget2.getFigure().suptitle('Sensor 2 Output',fontsize=16)
 		self.diffs2 = self.pltwidget2.getFigure().add_subplot(311)
+		self.diffs2.set_title('Differential Signal',fontsize=10)
+		self.diffs2.set_ylabel('Volts')
 		self.sums2 = self.pltwidget2.getFigure().add_subplot(312)
+		self.sums2.set_title('Sum Signal',fontsize=10)
+		self.sums2.set_ylabel('Volts')
 		self.imu2 = self.pltwidget2.getFigure().add_subplot(313)
+		self.imu2.set_title('IMU Data',fontsize=10)
+		self.imu2.set_ylabel('Volts')
 		self.diff_data2 = np.zeros((1,6))
 		self.sum_data2 = np.zeros((1,6))
 		self.imu_data2 = np.zeros((1,3))
-		self.layout.addWidget(self.pltwidget2)
+		self.lrLayout.addWidget(self.pltwidget2)
 
 		self.colors = ['#FF0000', '#FFA500', '#FFBF00', '#00FF00', '#FFFFFF'] # red, orange, yellow, green, white
 		self.acc1 = 4
 		self.acc2 = 4
-		self.window.setLayout(self.layout)
+
+		sub = rospy.Subscriber('continuous_data', self.data_subscriber)
+
+		self.window.setLayout(self.lrLayout)
 		self.window.show()
 
-	def paintEvent(self, e):
-		self.qp = QPainter()
-		self.qp.begin(self)
+	def update_sample_rate(self):
+		self.rate = float(self.sampling_rate_input.text())
 	def start_data_transfer_wrapper(self):
 		self.sensor.start_data_transfer(self.num)
 	def stop_data_transfer_wrapper(self):
@@ -100,34 +127,52 @@ class  gui(QWidget):
 	def device_num_selector_callback(self):
 		if self.device_num_selector.isChecked():
 			self.num = 1
+			self.device_num_selector.setText('Controlling Sensor 2')
 		else:
 			self.num = 0
+			self.device_num_selector.setText('Controlling Sensor 1')
 	def data_subscriber(self, msg):
 		if msg.sensor_num == 0:
 			self.diff_data1.append(msg.differentials)
+			if len(self.diff_data1) > 10000:
+				self.diff_data1 = self.diff_data1[-10000:]
 			self.sum_data1.append(msg.sum)
+			if len(self.sum_data1) > 10000:
+				self.sum_data1 = self.sum_data1[-10000:]
 			self.imu_data1.append(msg.imu)
+			if len(self.imu_data1) > 10000:
+				self.imu_data1 = self.imu_data1[-10000:]
 			self.acc1 = msg.accuracy
+
 		elif msg.sensor_num == 1:
 			self.diff_data2.append(msg.differentials)
+			if len(self.diff_data2) > 10000:
+				self.diff_data2 = self.diff_data2[-10000:]
 			self.sum_data2.append(msg.sum)
+			if len(self.sum_data2) > 10000:
+				self.sum_data2 = self.sum_data2[-10000:]
 			self.imu_data2.append(msg.imu)
+			if len(self.imu_data2) > 10000:
+				self.imu_data2 = self.imu_data2[-10000:]
 			self.acc2 = msg.accuracy
+		self.plotData()
+	def plotData(self):
+		self.diffs1.plot(diff_data1,color=self.colors[self.acc1])
+		self.diffs2.plot(diff_data2,color=self.colors[self.acc2])
 
-	def drawRectangle(self):
-		color = QColor(0,0,0)
+		self.sum1.plot(diff_sum1,color=self.colors[self.acc1])
+		self.sum2.plot(diff_sum2,color=self.colors[self.acc2])
 
-		color.setNamedColor(self.color[self.acc1])
-		self.qp.setBrush(color)
-		self.qp.drawRect(10,15,50,50)
+		self.imu1.plot(diff_imu1,color=self.colors[self.acc1])
+		self.imu2.plot(diff_imu2,color=self.colors[self.acc2])
 
-		color.setNamedColor(self.color[self.acc2])
-		self.qp.setBrush(color)
-		self.qp.drawRect(60,15,50,50)
+		self.pltwidget1.draw()
+		self.pltwidget2.draw()
 
 if __name__ == "__main__":
-	rospy.init_node('force_sensor_gui')
-
+	
 	sensor = Sensor(num_sensors=2)
 	app = QApplication([])
 	GUI = gui(sensor, app)
+
+	app.exec_()
